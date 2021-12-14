ed6adb55162b73acbf3f721cd44ce5b0e47292cd
pnina-miller <pninamiller@leader.codes>
2021-12-15 01:19:46 +0200

    add server

---
+++ server/server.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const app = express()
+const port = 8080
+const col= require('./data/tableColumns.json')
+const data= require('./data/dataSource.json')
+const details= require('./data/projectDetails.json')
+const reshimatShlavim= require('./data/shlavim.json')
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.get('/user/get', (req, res) => {console.log('user');
+const user={misparChativa:'22',misparAgaf:'99',kodSector:'99',misparMakal:'88'}
+  res.json({data:{userDataMf:user}})
+})
+app.get('/shofar/combobox', (req, res) => {console.log('shofar/combobox');
+  res.json({data:{projectStatusCombo:data}})
+})
+app.get('/shofar/shlavim/get', (req, res) => {console.log('shofar/shlavim');
+  res.json({"metadata":{"validations":{}}, "data":{"reshimatShlavimList":reshimatShlavim}}
+  )
+})
+app.get('/shofar/projects', (req, res) => {console.log('shofar/projects');
+  res.json(data)
+})
+app.get('/shofar/pirteyProject/get', (req, res) => {console.log('shofar/pirteyProject/get');
+  res.json({data:details})
+})
+app.get('/common/sectors', (req, res) => {console.log('sectors');
+res.json({data:'jj'})
+})
+app.get('/common/makals', (req, res) => {console.log('makals');
+res.json({data:'jj'})
+})
+app.get('/common/tables/tableColumnsByUser', (req, res) => {console.log('tableColumnsByUser');
+  res.json({data:col})
+})
+app.get('/common/agafim', (req, res) => {console.log('agafim');
+  res.json({data:{}})
+})
+app.get('/common', (req, res) => {console.log('common all');
+  res.json({data:'common all'})
+})
+app.get('/shofar/amalot', (req, res) => {console.log('amalot');
+  res.json({data:{amalot:{
+    misparProyectSagur: 'ssssstring',
+    kodAmala: 'ssssssstring',
+    shiurHncaAmalatTipul: 'asssstring',
+    shiurAmlatArvutSna: 'ssssstring',
+    shiurAmlArvLloHeter: 'ssssstring',
+    shiurAmlArvDiraOcls: 'ssssstring',
+    sachHakolAmalaNeto: 'ssssstring'
+  }}})
+})
+app.get('/shofar/natuney-mashkanta', (req, res) => {console.log('shofar all');
+
+  res.json({data:{natuneyMashkantaData: {
+    misparProyectSagur: 'any',
+    tar8HchvLeRsmMsknt: 'string',
+    tar8HchvLeRsmMskntDt: new Date(),
+    mezaheTikRmi: 'any',
+    kodDargatMashkanta: 'any',
+    teurDargatMsknt: 'any',
+    melelDargatMashkanta: 'any',
+    metegHagbala: 'any',
+    schumHitchayvutMsknt: 'any',
+  },
+    amalot: {
+      misparProyectSagur: 'any',
+      kodAmala: 'any',
+      shiurHncaAmalatTipul: 'any',
+      shiurAmlatArvutSna: 'any',
+      shiurAmlArvLloHeter: 'any',
+      shiurAmlArvDiraOcls: 'any',
+      sachHakolAmalaNeto: 'any'
+    },
+    dargatMashkantaOptLst:[{   id: 'string',
+      value: 'string',
+      shortDesc: 'string',
+      longDesc: 'string'},
+      {   id: 'string2',
+      value: 'string2',
+      shortDesc: 'string2',
+      longDesc: 'string2'}]
+  }})
+})
+app.get('/shofar/*', (req, res) => {console.log('shofar all');
+  res.json({data:'shofar all'})
+})
+app.get('/*', (req, res) => {console.log('all');
+  res.json({data:'all'})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
